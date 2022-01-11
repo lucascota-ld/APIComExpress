8a48a4adce058b3d4397f986c5d8971041394f6b
lucascota-id <lucas.a.cota@gmail.com>
2022-01-11 18:37:25 -0300

    manipulando a imagem no servidor

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,8 +1,12 @@
 const fs = require('fs')
 
-//LENDO A IMAGEM
-fs.createReadStream('./assets/salsicha.jpg')
-    //ESCREVENDO A IMAGEM QUE FOI LIDA
-    .pipe(fs.createWriteStream('./assets/salsicha-stream.jpg'))
-    //ESCUTANDO  O EVENTO
-    .on('finish', () => console.log('A imagem foi escrita com sucesso'))
+
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+
+    const novoCaminho = `./assets/imagens/${nomeDoArquivo}` 
+
+    fs.createReadStream(caminho)
+        
+        .pipe(fs.createWriteStream(novoCaminho))
+        .on('finish', () => callbackImagemCriada(novoCaminho))
+}
--- models/pets.js
+++ models/pets.js
@@ -1,17 +1,26 @@
 const conexao = require('../infraestrutura/conexao')
+const uploadDeArquivos = require('../arquivos/uploadDeArquivos')
 
 class Pet {
     adiciona(pet, res){
         const query = 'INSERT INTO Pets SET ?'
-        conexao.query(query, pet, erro => {
-            if(erro){
-                console.log(erro)
-                res.status(400).json(erro)
-            }else{
-                console.log('O pet foi adicionado')
-                res.status(200).json(pet)
+
+        uploadDeArquivos(pet.imagem, pet.nome, (novoCaminho) =>{
+
+            const novoPet = {
+                nome: pet.nome,
+                imagem: novoCaminho
             }
-        })
+            conexao.query(query, novoPet, erro => {
+                if(erro){
+                    console.log(erro)
+                    res.status(400).json(erro)
+                }else{
+                    console.log('O pet foi adicionado')
+                    res.status(200).json(novoPet)
+                }
+            })   
+        }) 
     }
 }
 module.exports = new Pet()
